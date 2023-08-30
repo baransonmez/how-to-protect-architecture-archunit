3faedb2437ed0443ca552c71601212abd4ff2e65
İhsan Baran SÖNMEZ <ibaranson@gmail.com>
2023-08-30 11:20:09 +0300

    initialize spring application

---
+++ src/main/java/dev/barans/howtoprotectarchitecturearchunit/HowToProtectArchitectureArchunitApplication.java
@@ -0,0 +1,13 @@
+package dev.barans.howtoprotectarchitecturearchunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToProtectArchitectureArchunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToProtectArchitectureArchunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/barans/howtoprotectarchitecturearchunit/HowToProtectArchitectureArchunitApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.barans.howtoprotectarchitecturearchunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToProtectArchitectureArchunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
